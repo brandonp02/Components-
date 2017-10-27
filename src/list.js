cd78ed5f432425c0ae3239b863671a2687844dd8
wavyb magic <Brandon Pichardo>
2017-10-26 20:56:23 -0400

    how to make Components

---
+++ src/list.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './card'
+import './list.css'
+
+
+export default function List() {
+  return (
+    <div className='list'>
+      <h3>Example List</h3>
+        <Card />
+        <Card />
+        <Card />
+    </div>
+  )
+}
